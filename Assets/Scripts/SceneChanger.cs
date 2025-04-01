bc63d04feb40d49f5f7d92dc1b4b1c7ada21626a
Miguel239g <miguel.ruiz@uniat.edu.mx>
2025-04-01 11:11:37 -0600

    Buttons added :o

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Remember to add the desired scenes to build settings
+public class SceneChanger : MonoBehaviour
+{
+    public string scene;
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
